d7237c644b5c85b71337f9040a929252997bac4c
Dubar Jérémy <jeremy.dubar@epitech.eu>
2021-10-24 16:05:20 +0200

    refacto

--- controller/user.validate.js
+++ controller/user.validate.js
@@ -3,11 +3,16 @@ const Joi = require("joi");
 module.exports.validateCreateUser = (req, res, next) => {
   const user = req.body;
 
-  // ça fonctionne alors que je l'appelle pas dans le controller, et ça me va mais je ne comprends pas pourquoi ? oO
   const schema = Joi.object({
     email: Joi.string().email().required(),
-    password: Joi.string().required(),
+    name: Joi.string().required(),
   });
 
-  return schema.validate(user);
+  try {
+    const { error } = schema.validate(user);
+    if (error) next(error);
+    next();
+  } catch (err) {
+    next(err);
+  }
 };
--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const userController = require("../controller/user");
+const { validateCreateUser2 } = require("../controller/user.validate");
 const router = express.Router();
 const auth = require("../middleware/auth");
 
-router.post("/", userController.createUser);
+router.post("/", validateCreateUser2, userController.createUser);
 
 router.get("/", auth, userController.protectedRoute);
 
